73ed94aef38d0cd6c8a0c2c1e95582d7b064bf86
Joel D'Autilio <joeldautilio@gmail.com>
2022-12-12 10:48:42 -0300

    Populate display on button click

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("#numpad button");
+let displayValue = "";
+
+for(const button of buttons) {
+  if(button.id == "clear" || button.id == "equals") continue;
+
+  button.addEventListener('click', handleClick);
+}
+
+function handleClick(event) {
+  displayValue += event.target.value;
+  display.textContent = displayValue;
+}
+
 const add = (a, b) => a+b;
 
 const subtract = (a, b) => a-b;
